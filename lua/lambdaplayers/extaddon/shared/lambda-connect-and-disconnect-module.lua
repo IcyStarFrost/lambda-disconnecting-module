5aedee88f0ce87443654391d2b81c9ca32d6af5f
IcyStarFrost <frostlegion10@gmail.com>
2023-01-27 14:03:20 -0600

    Updated to Lambda's new external addon system

--- lua/lambdaplayers/extaddon/shared/lambda-connect-and-disconnect-module.lua
+++ lua/lambdaplayers/extaddon/shared/lambda-connect-and-disconnect-module.lua
@@ -3,14 +3,12 @@ local color_white = color_white
 local rand = math.Rand
 local random = math.random
 
-local function Convars()
-    CreateLambdaConvar( "lambdaplayers_cd_showconnectmessage", 1, true, false, false, "If a join message should show in chat when a Lambda Player spawns", 0, 1, { type = "Bool", name = "Show Connect Message", category = "Misc" } )
-    CreateLambdaConvar( "lambdaplayers_cd_connectmessage", "connected to the server", true, false, false, "The message to show when a Lambda Player spawns", nil, nil, { type = "Text", name = "Connect Text", category = "Misc" } )
+CreateLambdaConvar( "lambdaplayers_cd_showconnectmessage", 1, true, false, false, "If a join message should show in chat when a Lambda Player spawns", 0, 1, { type = "Bool", name = "Show Connect Message", category = "Misc" } )
+CreateLambdaConvar( "lambdaplayers_cd_connectmessage", "connected to the server", true, false, false, "The message to show when a Lambda Player spawns", nil, nil, { type = "Text", name = "Connect Text", category = "Misc" } )
+CreateLambdaConvar( "lambdaplayers_cd_allowdisconnecting", 1, true, false, false, "If Lambda Players are allowed to disconnect", 0, 1, { type = "Bool", name = "Allow Disconnecting", category = "Misc" } )
+CreateLambdaConvar( "lambdaplayers_cd_disconnectmessage", "disconnected from the server", true, false, false, "The message to show when a Lambda Player disconnects", nil, nil, { type = "Text", name = "Disconnect Text", category = "Misc" } )
+CreateLambdaConvar( "lambdaplayers_cd_disconnecttime", 5000, true, false, false, "The max amount of time it can take for a Lambda to disconnect", 15, 5000, { type = "Slider", decimals = 0, name = "Disconnect Time", category = "Misc" } )
 
-    CreateLambdaConvar( "lambdaplayers_cd_allowdisconnecting", 1, true, false, false, "If Lambda Players are allowed to disconnect", 0, 1, { type = "Bool", name = "Allow Disconnecting", category = "Misc" } )
-    CreateLambdaConvar( "lambdaplayers_cd_disconnectmessage", "disconnected from the server", true, false, false, "The message to show when a Lambda Player disconnects", nil, nil, { type = "Text", name = "Disconnect Text", category = "Misc" } )
-    CreateLambdaConvar( "lambdaplayers_cd_disconnecttime", 5000, true, false, false, "The max amount of time it can take for a Lambda to disconnect", 15, 5000, { type = "Slider", decimals = 0, name = "Disconnect Time", category = "Misc" } )
-end
 
 -- This is all very simple. I don't really need to put a lot of documentation on this
 
@@ -75,6 +73,5 @@ local function Think( self )
 end
 
 hook.Add( "LambdaOnThink", "lambdadisconnecting_think", Think )
-hook.Add( "LambdaOnConvarsCreated", "lambdadisconnecting_convars", Convars )
 hook.Add( "LambdaAIInitialize", "lambdadisconnecting_AIinit", AIInitialize )
 hook.Add( "LambdaOnInitialize", "lambdadisconnecting_init", Initialize )
